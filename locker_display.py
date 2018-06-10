7e659f8cdd33605f575fda93b09415c36e5afa8b
gabrielcoder <gabrielcoder247@gmail.com>
2018-06-10 20:19:34 +0300

    Function to create a new account

--- locker_display.py
+++ locker_display.py
@@ -1,3 +1,16 @@
 import pyperclip
 from credentials_data import Credential # Importing the credential class
 from user_data import User # Importing the user class
+
+def create_user(fname,lname,password):
+	'''
+	Function to create a new user account
+	'''
+	new_user = User(fname,lname,password)
+	return new_user
+    
+def save_user(user):
+	'''
+	Function to save a new user account
+	'''
+	User.save_user(user)    
