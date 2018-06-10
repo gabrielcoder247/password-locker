4c654352604b29edec03c73c7050ee254e6736e3
gabrielcoder <gabrielcoder247@gmail.com>
2018-06-10 14:13:22 +0300

    import credential and user classes

--- credentials_data_test.py
+++ credentials_data_test.py
@@ -1,5 +1,6 @@
 import unittest # Importing the unittest module
-from credntials_data import Credentials # Importing the contact class
+from credentials_data import Credential # Importing the credential class
+from user_data import User # Importing the user class
 
 class TestCredentials(unittest.TestCase):
 	'''
@@ -11,9 +12,9 @@ class TestCredentials(unittest.TestCase):
 		'''
 		Function to test whether the login in function check_user works as expected
 		'''
-		self.new_user = User('Gabriel','Nwachukwu,'moringa123')
+		self.new_user = User('Gabriel','Nwachukwu','moringa123')
 		self.new_user.save_user()
-		user2 = User('Roland','Nwachukwu,'yola123')
+		user2 = User('Roland','Nwachukwu','yola123')
 		user2.save_user()
 
 		for user in User.users_list:
@@ -22,3 +23,22 @@ class TestCredentials(unittest.TestCase):
 		return current_user
 
 		self.assertEqual(current_user,Credential.check_user(user2.password,user2.first_name))
+    
+	def setUp(self):
+		'''
+		Function to create an account's credentials before each test
+		'''
+		self.new_credential = Credential('Mary','Facebook','maryjoe','nairobi123')
+
+	def test__init__(self):
+		'''
+		Test to if check the initialization/creation of credential instances is properly done
+		'''
+		self.assertEqual(self.new_credential.user_name,'Mary')
+		self.assertEqual(self.new_credential.site_name,'Facebook')
+		self.assertEqual(self.new_credential.account_name,'maryjoe')
+		self.assertEqual(self.new_credential.password,'nairobi123')        
+
+if __name__ == '__main__':
+    unittest.main()        
+    
--- user_data_test.py
+++ user_data_test.py
@@ -71,11 +71,7 @@ class TestCredentials(unittest.TestCase):
 
 		self.assertEqual(current_user,Credential.check_user(user2.password,user2.first_name))
 
-    def setUp(self):
-		'''
-		Function to create an account's credentials before each test
-		'''
-		self.new_credential = Credential('peter','Facebook','okoye','nigeria123')    
+      
            
 
 if __name__ == '__main__':
